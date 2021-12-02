a42ea97649bef539fa5a4da558b496dca8cde094
Cameron Scott <camscott678@gmail.com>
2021-12-02 08:32:16 -0500

    Day 2 Part 2

---
+++ day2/part2/main.py
@@ -0,0 +1,51 @@
+import argparse
+
+
+def parse_file(filename):
+    file = open(filename, "r")
+    return file.readlines()
+
+
+class Submarine():
+    def __init__(self):
+        self.horizontal = 0
+        self.depth = 0
+        self.aim = 0
+
+    def forward(self, units):
+        self.horizontal += units
+        self.depth += units * self.aim
+
+    def up(self, units):
+        self.aim -= units
+
+    def down(self, units):
+        self.aim += units
+
+    def printLocation(self):
+        print("The submarine has a horizontal position of {} and a depth of {}. These multiply to {}.".format(self.horizontal, self.depth, self.horizontal*self.depth))
+
+    def followDirections(self, sequence):
+        for instruction in sequence:
+            seq_list = instruction.split(" ")
+            direction = seq_list[0]
+            distance = int(seq_list[1])
+
+            if direction == "forward":
+                self.forward(distance)
+            elif direction == "up":
+                self.up(distance)
+            elif direction == "down":
+                self.down(distance)
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-f", "--filename", type=str, default="training_input.txt")
+    args = parser.parse_args()
+
+    sequence = parse_file(args.filename)
+    
+    s = Submarine()
+    s.followDirections(sequence)
+    s.printLocation()
