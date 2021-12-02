63b1a261358134569cce86e95f33bf5c75d1b869
Cameron Scott <camscott678@gmail.com>
2021-12-01 21:57:29 -0500

    Updated D1P2 to contain two algorithm versions

--- day1/part2/main.py
+++ day1/part2/main.py
@@ -6,18 +6,37 @@ def parse_file(filename):
     return file.readlines()
 
 
+class SlidingWindowAlgorithm():
+    def listComprehension(sequence):
+        # List comprehension version
+        triple_sum = lambda idx : int(sequence[idx]) + int(sequence[idx+1]) + int(sequence[idx+2])
+        print([1 for idx in range(len(sequence)-3) if triple_sum(idx) < triple_sum(idx+1)])
+        count = sum([1 for idx in range(len(sequence)-3) if triple_sum(idx) < triple_sum(idx+1)])
+        
+        print("The count with list comprehension is:", count)
+
+
+    def iterativeMethod(sequence):
+        # Iterative version
+        count = 0
+        for idx in range(len(sequence)-3):
+            sum1 =  int(sequence[idx]) + int(sequence[idx+1]) + int(sequence[idx+2])
+            sum2 =  int(sequence[idx+1]) + int(sequence[idx+2]) + int(sequence[idx+3])
+            if sum2 > sum1:
+                count = count + 1
+
+        print("The count with for-loop iteration is:", count)
+
+
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("-f", "--filename", type=str, default="training_input.txt")
+    parser.add_argument("-a", "--alg", type=str, default="list_comp", choices=["list_comp", "iterative"])
     args = parser.parse_args()
 
     sequence = parse_file(args.filename)
-
-    count = 0
-    for idx in range(len(sequence)-3):
-        sum1 =  int(sequence[idx]) + int(sequence[idx+1]) + int(sequence[idx+2])
-        sum2 =  int(sequence[idx+1]) + int(sequence[idx+2]) + int(sequence[idx+3])
-        if idx != 0 and sum2 > sum1:
-            count = count + 1
-
-    print("The count is:", count)
+    
+    if args.alg == "list_comp":
+        SlidingWindowAlgorithm.listComprehension(sequence)
+    elif args.alg == "iterative":
+        SlidingWindowAlgorithm.iterativeMethod(sequence)
